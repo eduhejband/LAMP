4b5b5cc59d446e4509625c0a84dd30b4bf5b464c
Eduardo Hejazi Bandeira <106497109+eduhejband@users.noreply.github.com>
2023-03-07 00:54:53 -0300

    Update

--- info.php
+++ info.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "2023";
+$password = "***";
 $dbname = "lamp";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
